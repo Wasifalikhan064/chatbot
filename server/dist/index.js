d805d27aeb2ade69f4987981397f81ff00224499
Wasifalikhan064 <wasifalikhan064@gmail.com>
2025-07-15 13:57:24 +0530

    tsc build

--- server/dist/index.js
+++ server/dist/index.js
@@ -10,6 +10,7 @@ const dbConnect_1 = require("./lib/dbConnect");
 const compilerRouter_1 = require("./routes/compilerRouter");
 const userRouter_1 = require("./routes/userRouter");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+(0, dotenv_1.config)();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
@@ -17,7 +18,6 @@ app.use((0, cors_1.default)({
     credentials: true,
     origin: ["http://localhost:5173", process.env.CLIENT_URL],
 }));
-(0, dotenv_1.config)();
 app.use("/compiler", compilerRouter_1.compilerRouter);
 app.use("/user", userRouter_1.userRouter);
 (0, dbConnect_1.dbConnect)();
